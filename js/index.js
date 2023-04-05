e8910d01a796ce86de9b8f7f9782ab5742871b0c
agusrahmata <agusrahmat.akmaluddin@gmail.com>
2023-04-05 22:27:09 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+//javascript
+
+//menghitung luas segitiga
+function calculateLuasSegitiga (){
+    const alassegitiga = document.getElementById("alassegitiga").value;
+    const tinggisegitiga = document.getElementById("tinggisegitiga").value;
+    const answer = alassegitiga * tinggisegitiga / 2;
+    document.getElementById("inputluassegitiga").value = answer;
+}
+
+//menghitung keliling segitiga
+function calculateKelilingSegitiga (){
+    const asegitiga = document.getElementById("a-segitiga").value;
+    const bsegitiga = document.getElementById("b-segitiga").value;
+    const csegitiga = document.getElementById("c-segitiga").value;
+    const answer = Number(asegitiga) + Number(bsegitiga) + Number(csegitiga);
+    document.getElementById("inputkelilingsegitiga").value = answer;
+}
+
+//menghitung luas jajargenjang
+function calculateLuasJajargenjang (){
+    const alasjajargenjang = document.getElementById("alasjajargenjang").value;
+    const tinggijajargenjang = document.getElementById("tinggijajargenjang").value;
+    const answer = alasjajargenjang * tinggijajargenjang;
+    document.getElementById("inputluasjajargenjang").value = answer;
+}
+
+//menghitung keliling jajargenjang
+function calculateKelilingJajargenjang (){
+    const ajajargenjang = document.getElementById("a-jajargenjang").value;
+    const bjajargenjang = document.getElementById("b-jajargenjang").value;
+    const answer = 2 * ( Number(ajajargenjang) + Number(bjajargenjang) );
+    document.getElementById("inputkelilingjajargenjang").value = answer;
+}
